c78343da00c64421e9c5423c331fc4f378acf9b9
Terer <terermike3@gmail.com>
2024-04-18 21:28:08 +0300

    Added Task and Newt\Task componets to handle adding of tasks into projects

--- src/App.jsx
+++ src/App.jsx
@@ -49,11 +49,25 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== project.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectState.projects.find(
     (project) => project.id === projectState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectState.selectedProjectId === null) {
     content = (
---
+++ src/components/NewTask.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function NewTask() {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  return (
+    <div className="flex items-center gap-4 ">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 roundeed-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      ></input>
+      <button className="text-stone-700 hover:text-stone-950"> Add Task</button>
+    </div>
+  );
+}
